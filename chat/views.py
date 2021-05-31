3f5d0c61efb1d9bd7403b5a155d7b06a1a326a72
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-05-31 15:03:00 +0530

    ASGI, Templates setup ...

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request , 'index.html' , {})
+
+def room(request):
+    return render(request , 'chatroom.html' , {})
