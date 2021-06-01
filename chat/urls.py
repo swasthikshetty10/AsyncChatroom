ac139177a5c2a3f6dd4fa47bf424471f9a51be85
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-06-01 23:11:22 +0530

    some styles

--- chat/urls.py
+++ chat/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [ 
     path ("" , views.home , name = "home"),
-    path('<str:room_name>/' , views.room , name = "room"),
+    path('chat/<str:room_name>/' , views.room , name = "room"),
 ]
