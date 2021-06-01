61bcacc4145c739264021fe1a82d7ce5e90ffeb7
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-06-01 17:55:03 +0530

    WS send and recive✔️

--- chat/routing/consumers.py
+++ chat/routing/consumers.py
@@ -1,7 +1,7 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-
+import time
 class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         groups = ["broadcast"]
@@ -20,9 +20,34 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             {
                 'type' : "test_msg",
-                "tester" : "hello world"
+                "tester" : "web socket connected"
+            }
+            
+        )
+ 
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type' : "test_msg",
+                "tester" : "web socket connected"
+            }
+            
+        )
+    async def receive(self , text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']   
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type' : "chat_message",
+                "message" : message
             }
         )
+    async def chat_message(self , event) :
+        message = event['message']
+        await self.send(text_data = json.dumps({
+            'message' :  message
+        }))
     async def test_msg(self , event) :
         tester = event['tester']
         await self.send(text_data = json.dumps({
