6202cfe267820226fe7f9d6f48f473cf37afaa92
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-06-02 15:40:55 +0530

    user session

--- chat/views.py
+++ chat/views.py
@@ -3,17 +3,26 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseRedirect
 # Create your views here.
 
-username = None
+
 
 def home(request):
-    return render(request , 'index.html')
+    if request.session.get('user_name', None):
+        return render(request , 'index.html',  {'room_name' : "" , "user_name" : request.session['user_name']})
+    if request.session.get('room_name', None):
+        return render(request , 'index.html',  {'room_name' : request.session['room_name'] , "user_name" : "" })
+    
+    return render(request , 'index.html' , {'room_name' : "" , 'user_name' : ""})   
 @csrf_exempt
 def chat(request):
     content = request.POST
-    global username
-    username = content['user_name']
+    request.session['user_name'] = content['user_name']    
     return HttpResponseRedirect(f"/chat/{content['room_name']}")
 
 def room(request , room_name):
-    return render(request , 'chatroom.html' , {'room_name' : room_name , "username" : username})
+    request.session['room_name'] = room_name
+    if not request.session.get('user_name', None) :
+        return HttpResponseRedirect("/" , {'room_name' : room_name})
+    return render(request , 'chatroom.html' , {'room_name' : room_name , "username" : request.session['user_name']})
     
+def changename(request):
+    content = request.POST
--- core/settings/settings.py
+++ core/settings/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -87,7 +88,6 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
