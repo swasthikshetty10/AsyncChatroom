91219a1c9d165c86eb9ce4a023ad915f1148410a
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-06-02 11:10:06 +0530

    🔷style

--- chat/urls.py
+++ chat/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [ 
     path ("" , views.home , name = "home"),
+    path('chat/' , views.chat , name = "chat"),
     path('chat/<str:room_name>/' , views.room , name = "room"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect
 # Create your views here.
+
+username = None
+
 def home(request):
-    return render(request , 'index.html' , {})
+    return render(request , 'index.html')
+@csrf_exempt
+def chat(request):
+    content = request.POST
+    global username
+    username = content['user_name']
+    return HttpResponseRedirect(f"/chat/{content['room_name']}")
 
 def room(request , room_name):
-    return render(request , 'chatroom.html' , {'room_name' : room_name , "username" : str(request.user)})
+    return render(request , 'chatroom.html' , {'room_name' : room_name , "username" : username})
+    
